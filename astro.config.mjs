f1cb4d96e32f337dcfef571f10901bc16d3cc075
brittneypostma <bdesignedwebdev@gmail.com>
2022-11-15 12:09:54 -0500

    init

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import image from "@astrojs/image"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/edge-functions"
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astro-moon-landing.netlify.app/",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  },
+  output: "server",
+  adapter: netlify()
+})
